e20c9570ce5cc3ce0153a1b334ce079c9124c9e0
WilliamDrap <wdrapeaud@orange.fr>
2023-01-13 14:49:04 +0100

    sideral day view (don't work)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import PyStella
-import pyautogui
-from PIL import Image
+#import pyautogui
+#from PIL import Image
 
 time = PyStella.pyStellaTime
 stellarium = PyStella.pyStellarium
@@ -8,20 +8,18 @@ constellation = PyStella.pyStellaConstellation
 location = PyStella.pyStellaLocation
 request = PyStella.requestServer
 
+constellation.reset_all_property()
+
 time.setTimeUtc(year=2022, month=12, day=23, hour=21, minute=0, second=0)
 time.setTimeRate(1)
 location.set(latitude=50.27, longitude=2.89, elevation=110)
 stellarium.set_field_of_view(90)
-stellarium.move_altaz(azimut=0, altitude=10).wait(3)
-
-
-constellation.reset_all_property()
-
+stellarium.move_altaz(azimut=135, altitude=40).wait(3)
 
 stellarium.set_gui_visible(False)
 stellarium.select_object_by_name(object_name='orion', mode='center')
 
-stellarium.wait(pause_in_sec=3)
+stellarium.wait(pause_in_sec=4)
 
 constellation.set_property(linedisplayed=True, isolateselected=True)
 
@@ -31,10 +29,13 @@ constellation.set_property(artdisplayed=True, linedisplayed=False)
 stellarium.wait(pause_in_sec=3)
 
 stellarium.select_object_by_name(object_name='canis major', mode='mark')
-stellarium.set_field_of_view(75)
-stellarium.wait(pause_in_sec=1)
+#stellarium.set_field_of_view(75)
 # im1=pyautogui.screenshot('stellarium.png')
 stellarium.wait(pause_in_sec=3)
+constellation.set_property(artdisplayed=False, linedisplayed=False)
+stellarium.select_object_by_name(object_name='m42', mode='center')
+stellarium.set_field_of_view(3.8)
+stellarium.wait(pause_in_sec=10)
 
 stellarium.set_gui_visible(True)
 # stellarium.setFieldOfView(90)
